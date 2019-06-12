97dbc768ff07b7e1b8569797f416edc580fc3f19
zemken12 <zemken12@gmail.com>
2019-06-12 01:04:07 -0400

    Update ddp.py
    final script

--- ddp.py
+++ ddp.py
@@ -1,28 +1,31 @@
-# I played around with this idea and the way I can see someone doing it is getting a list of MXD file paths, 
-# creating Map Document objects, and then appending those objects to a list based on whether Data Drive Pages are enabled.
-# Depending on what you plan on doing next, you may want to pull the file paths or MXD titles, 
-# and create a new list that are just unicode objects instead of MXDs. Hope this helps!
- 
+# Shelby Zemken 06/11/2019    zemken12@gmail.com
+# This script prompts a user for an input directory containing MXDs,
+# and prompts for an output directory where PDFs are batch exported
+# This script exports both normal MXDs and MXDs with data driven pages enabled.
 
-import arcpy
-arcpy.env.workspace = r"C:\Users\zemke\Desktop\EDR\mxd_test"
-list = arcpy.ListFiles("*.mxd")
- 
-mxdnames = []
-for item in list:
-    print (str(item))
-    mxdnames.append(str(item))
- 
-mxds = []
-for file in mxdnames:
-    mxd = arcpy.mapping.MapDocument(r"{}\{}".format(arcpy.env.workspace,file))
-    mxds.append(mxd)
- 
-falselist = []
-for m in mxds:
-    print (type(m))
-    if m.isDDPEnabled == False:    
-    falselist.append(m)
- 
-for map in falselist:
-    print (map.filePath)
+import os, arcpy
+
+# user sets input and output directories
+input_dir = arcpy.GetParameterAsText(0)
+output_dir = arcpy.GetParameterAsText(1)
+arcpy.env.workspace = ws = str(input_dir)
+mxdnames = arcpy.ListFiles("*.mxd")
+
+for documentname in mxdnames:
+    input = os.path.join(ws, documentname)
+    mxd = arcpy.mapping.MapDocument(input)
+    output_name = (os.path.join(output_dir, documentname[:-4]) + ".pdf")
+    if hasattr(mxd, "dataDrivenPages"):
+        ddp = mxd.dataDrivenPages
+        ddp.exportToPDF(output_name, "ALL")
+        arcpy.AddMessage('\t' + str(mxd) + str(' exported successfully (Data Driven Pages Enabled).'))
+        del mxd
+    else:
+        arcpy.mapping.ExportToPDF(mxd, output_name)
+        arcpy.AddMessage('\t' + str(mxd) + str(' exported successfully.'))
+        del mxd
+
+arcpy.AddMessage(('Exports complete!'))
+complete = os.startfile(output_dir)
+complete
+
